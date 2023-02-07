41a89901db0d51d0ef841bfb982ae209ca08dc38
giuliaballestrero <giulia.ballestrero@gmail.com>
2023-02-07 15:45:44 +0100

    Bonus: add another page

--- resources/views/cat.blade.php
+++ resources/views/cat.blade.php
@@ -4,20 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>Bongo Cat</title>
 </head>
 <body>
-    <header>
-        
-        <h1>
-            Hello World! &star;
-        </h1>
 
-        <a href="http://127.0.0.1:8000/cat">
-            -> Click here to see the bongo cat <-
-        </a>
-
-</header>
+    <main>
+        <img src="https://media.tenor.com/NjbLQCvQoC8AAAAC/bongo-cat.gif" alt="bongo cat">
+    </main>
     
 </body>
 </html>
